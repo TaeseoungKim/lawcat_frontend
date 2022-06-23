0df05dcf52d74717a698a08a48bdda4e4746f19c
Taeseong <aka404365@gmail.com>
2022-06-24 07:31:08 +0900

    [refactor] refactor

--- src/components/LawcatHeader/LawcatHeader.js
+++ src/components/LawcatHeader/LawcatHeader.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 import home from '../../images/home.png';
 import lawchat from '../../images/lawchat.png';
 import customerservice from '../../images/customerservice.png';
-import {useEffect, useRef, useState} from "react";
-import styled from 'styled-components'
+import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 
 const LawcatHeader = () => {
   const currentUrl = window.location.pathname;
@@ -27,73 +27,68 @@ const LawcatHeader = () => {
   //   { link: '/LoginTest', title: localStorage.getItem("userId") || '로그인'   },
   // ];
 
-// 로그인 안하면, 로그인 페이지로
-  const userId = localStorage.getItem("userId");
+  // 로그인 안하면, 로그인 페이지로
+  const userId = localStorage.getItem('userId');
 
   const MenuBox = [
     { link: userId ? '/Lawchat' : '/Login', title: '법률 상담' },
-    { link: userId ? '/CustomerService' :'/Login', title: '고객문의' },
-    { link: userId ? '/Login': '/Login', title: userId || '로그인'   },
+    { link: userId ? '/CustomerService' : '/Login', title: '고객문의' },
+    { link: userId ? '/Login' : '/Login', title: userId || '로그인' },
   ];
 
   const handleLogout = (e) => {
-    if(e.target.title !== '법률 상담' && e.target.title !== "고객문의" && userId){
-      e.preventDefault()
-      localStorage.removeItem("userId")
-      localStorage.removeItem("token")
-      window.location.href = "/"
-
+    if (
+      e.target.title !== '법률 상담' &&
+      e.target.title !== '고객문의' &&
+      userId
+    ) {
+      e.preventDefault();
+      localStorage.removeItem('userId');
+      localStorage.removeItem('token');
+      window.location.href = '/';
     }
-
-  }
+  };
   const MenuList = MenuBox.map((menu, idx) => (
-      <li
-          className="App_MenuList"
-          key={idx}
-          style={menu.title === '로그인' ? { fontWeight: 'bold' } : null}
+    <li
+      className="App_MenuList"
+      key={idx}
+      style={menu.title === '로그인' ? { fontWeight: 'bold' } : null}
+    >
+      <NavLink
+        className="App_MenuList_NavLink"
+        exact
+        to={menu.link}
+        activeClassName="active"
+        title={menu.title}
+        onClick={handleLogout}
       >
-        <NavLink
-            className="App_MenuList_NavLink"
-            exact
-            to={menu.link}
-            activeClassName="active"
-            title={menu.title}
-            onClick={handleLogout}
-        >
-          {/* <img
-          className="App_MenuList_img"
-          src={returnImgName(menu.title)}
-          alt={menu.title}
-        /> */}
-          {menu.title}
-        </NavLink>
-
-
-      </li>
+        {menu.title}
+      </NavLink>
+    </li>
   ));
 
   return (
-      <>
-        <div id="LawcatHeader">
-          <NavLink exact to="/" activeClassName="active">
-            <img id="header_lawcat" src={header_lawcat} alt="header lawcat"></img>
-            <img
-                id="header_styledcat"
-                src={styledcat}
-                alt="header styledcat"
-            ></img>
-          </NavLink>
-          <ul className="App_tabs">{MenuList}</ul>
-        </div>
-      </>
+    <>
+      <div id="LawcatHeader">
+        <NavLink exact to="/" activeClassName="active">
+          <img id="header_lawcat" src={header_lawcat} alt="header lawcat"></img>
+          <img
+            id="header_styledcat"
+            src={styledcat}
+            alt="header styledcat"
+          ></img>
+        </NavLink>
+        <ul className="App_tabs">{MenuList}</ul>
+      </div>
+    </>
   );
 };
 
 const LoginErr = styled.h1`
   position: absolute;
-  top: 10%;  
+  top: 10%;
   left: 47%;
-  
+
   color: red;
-`
+`;
 export default LawcatHeader;
